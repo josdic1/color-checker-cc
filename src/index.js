138455551520b43c815c55b243e92a7ade952fe9
josdic1 <joshuadicker@gmail.com>
2025-02-01 15:36:13 -0500

    updated stylesheet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 
 const init = () => {
 
+  /** --------------------- DOM Elements--------------------- **/
+
+  const cube = document.getElementById("color-cube")
+  const list = document.getElementById("color-list")
+
   /** --------------------- STATEFUL VARS--------------------- **/
   let isLoading = true
   let inEditMode = false
@@ -10,10 +15,67 @@ const init = () => {
   let selectedColor = { id: '', title: '', code: '' }
 
   /** --------------------- INIT FETCH --------------------- **/
+
   fetchColors()
+
   /** ---------------------EVENT HANDLERS  --------------------- **/
 
+
+
   /** --------------------- 🎨 RENDER FUNCTIONS --------------------- **/
+
+  function renderColorCube() {
+
+
+    const cubeHtml =
+      `   <h1 id="colorCube" style="{ 'color': 'red'}">COLOR</h1>`
+
+    cube.innerHTML = cubeHtml
+  }
+
+
+
+  function renderColorList(data) {
+
+    const listMap = data.map(item => (
+      `<tr id=${item.id}>
+        <td>${item.id}</td>
+         <td>${item.title}</td>
+          <td>${item.code}</td>
+          <td>
+            <button type='button' button id='view-${item.id}'>View</button>
+          </td>
+          <td>  
+            <button type='button' button id='view-${item.id}'>Edit</button>
+          </td>
+          <td>  
+            <button type='button' button id='view-${item.id}'>Del</button>
+          </td>
+      </tr>`
+    ))
+
+    const listHtml =
+      `<table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Color</th>
+            <th>Code</th>
+            <th>View</th>
+            <th>Edit</th>
+            <th>Del</th>
+            <th>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          ${listMap}
+        </tbody>
+      </table>`
+
+    list.innerHTML = listHtml
+  }
+
 
 
   /** --------------------- 🎯 HANDLER FUNCTIONS --------------------- **/
@@ -23,6 +85,7 @@ const init = () => {
 
 
   /** --------------------- 📡 API CALLS --------------------- **/
+
   async function fetchColors() {
     try {
       const r = await fetch(`http://localhost:3000/colors`)
@@ -30,7 +93,9 @@ const init = () => {
         throw new Error('Bad GET response')
       }
       const data = await r.json()
-      console.log(data)
+      renderColorCube()
+      renderColorList(data)
+      colors = data
     } catch (error) { console.error('GET: ', error) }
   }
 
